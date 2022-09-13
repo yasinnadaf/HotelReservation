b457c187d11b611e8709566c323ae47131db0794
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-13 18:13:01 +0530

    cheapest hotel

--- src/main/java/com/bridgelabz/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -1,11 +1,29 @@
 package com.bridgelabz.hotelreservation;
 
 public class Hotel {
-    String name;
-    int rates;
+    private final String name;
+    private final int rate;
+    private int totalCost;
 
-    public Hotel(String name, int rates) {
+    public Hotel(String name, int rate) {
         this.name = name;
-        this.rates = rates;
+        this.rate = rate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+
 }
--- src/main/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -1,10 +1,24 @@
 package com.bridgelabz.hotelreservation;
 
+import java.util.Scanner;
+
 public class HotelReservationMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation program");
+        Scanner scanner =new Scanner(System.in);
         HotelReservationService hotelReservationService =new HotelReservationService();
         hotelReservationService.addHotel(new Hotel("Lakewood",110));
+        hotelReservationService.addHotel(new Hotel("Bridgewood", 160));
+        hotelReservationService.addHotel(new Hotel("Ridgewood", 220));
+
+        System.out.println("Enter check-in date: (dd-mm=yyyy) ex. 01-01-2000");
+        String checkInDate = scanner.nextLine();
+        System.out.println("Enter check-out date: (dd-mm=yyyy)");
+        String checkOutDate = scanner.nextLine();
+        Hotel cheapestHotel = hotelReservationService.getCheapestHotel(checkInDate, checkOutDate);
+        System.out.println("Cheapest Hotel: " + cheapestHotel.getName() + ", Total Cost: " + cheapestHotel.getTotalCost());
     }
+
+
 }
